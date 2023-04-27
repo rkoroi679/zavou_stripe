99cbec9c36c5c8c4531c9b625fabe2a7f127bd75
rusi <rkoroi15@gmail.com>
2023-04-27 15:35:57 +1000

    Create connection to stripe

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+session_start();
+
+require 'service/stripe_connection.php';
+
+create_stripe_client();
+
+?>
--- service/stripe_connection.php
+++ service/stripe_connection.php
@@ -1,6 +1,15 @@
 <?php
 require 		"vendor/autoload.php";
 
+function create_stripe_client()
+{
+    $stripe = new \Stripe\StripeClient('sk_test_51MyQ1PDMB4K8I2vgYkE5FNOKKDcoczhhrK7x6CJcT0Zm7SjLUWp0aGv3h59VBhmk6rcl3T7FRnOXAVSCHMh650MT00WpMhujK2');
+    $customer = $stripe->customers->create([
+        'description' => 'New Customer',
+        'email' => 'rusi@example.com',
+        'payment_method' => 'pm_card_visa',
+    ]);
+    echo $customer;   
 
-
+}
 ?>
